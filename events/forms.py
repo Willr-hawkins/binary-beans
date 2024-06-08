9c6ef7fb5ef8ed1af0a8649b0f557618ae27f6d9
William Hawkins <Hawkinswill02@gmail.com>
2024-06-08 13:36:31 +0000

    Style the booking form on the events page

--- events/forms.py
+++ events/forms.py
@@ -1,7 +1,15 @@
 from .models import BookingRequest
 from django import forms
 
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class BookingForm(forms.ModelForm):
     class Meta:
         model = BookingRequest
         fields = ('customer_name', 'customer_email', 'booking_date',)
+        widgets = {'booking_date': DateInput,}
+        
+
+       
