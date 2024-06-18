565eb94f89fd0e89ff796496fa9434ccb6b907c0
William Hawkins <Hawkinswill02@gmail.com>
2024-06-18 12:34:27 +0000

    Add funcitonality for editing and deleting reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.review_list, name="reviews"),
+    path('review/edit/<int:review_id>/', views.edit_review, name='edit_review'),
+    path('review/delete/<int:review_id>/', views.delete_review, name='delete_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from django.views import generic
 from .models import Review
 from .forms import ReviewForm
@@ -26,3 +27,28 @@ def review_list(request):
             "review_count": review_count
         }
     )
+
+def edit_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id, review_name=request.user)
+
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Review updated successfully!")
+            return redirect('reviews')
+
+    else:
+        form = ReviewForm(instance=review)
+
+    return render(request, 'reviews/edit_review.html', {'form': form, 'review': review})
+
+def delete_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id, review_name=request.user)
+
+    if request.method == "POST":
+        review.delete()
+        messages.success(request, "Booking request deleted successfully!")
+        return redirect('reviews')
+
+    return render(request, 'reviews/delete_review.html', {'review': review})
