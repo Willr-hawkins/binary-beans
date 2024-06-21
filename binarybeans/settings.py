a18586cd381623fe62a211c003828431a961de65
William Hawkins <Hawkinswill02@gmail.com>
2024-06-21 10:17:07 +0000

    Add custom 500 error page

--- binarybeans/settings.py
+++ binarybeans/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-willrhawkin-binarybeans-qeeyit71v12.ws.codeinstitute-ide.net',
+ALLOWED_HOSTS = ['8000-willrhawkin-binarybeans-wtttti04kh3.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
 
 
